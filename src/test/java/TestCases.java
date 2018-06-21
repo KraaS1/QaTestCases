32bbade7ccb314d4228d940520c85e99632f68c4
KraaS1 <ilya.krasyuk.1997@gmail.com>
2018-06-21 15:14:48 +0300

    1st testcase

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,8 +1,76 @@
+import com.codeborne.selenide.Configuration;
+import io.restassured.response.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+
+import java.util.List;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.restassured.RestAssured.given;
+import static io.restassured.path.json.JsonPath.from;
+import static org.junit.Assert.assertEquals;
+
 
 public class TestCases {
+    private static final String BASE_URL_API = "https://api.github.com/search/repositories";
+    private static final String BASE_URL = "https://github.com/";
+
+    @Before
+    public void SetUP() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        Configuration.browser = "chrome";
+        open(BASE_URL);
+    }
+
     @Test
-    public void testCase1(){
-        System.out.println("TestCase1");
+    public void testCase1() throws JSONException {
+        Response response = given().when().
+                get("https://api.github.com/search/repositories?q=selenide&sort=stars");
+        assertEquals(200, response.getStatusCode());
+        System.out.println(response.getStatusCode());
+        List<String> Name = from(response.asString()).get("items.name");
+        List<String> Opys = from(response.asString()).get("items.description");
+        List<String> language = from(response.asString()).get("items.language");
+        List<String> license = from(response.asString()).get("items.license.name");
+        List<Integer> star = from(response.asString()).get("items.stargazers_count");
+        SetUP();
+        $(By.xpath("/html/body/div[1]/header/div/div[2]/div/div/div/div/form/label/input[1]"))
+                .setValue("Selenide").pressEnter();
+
+
+        String namerepos = $(By.xpath("//*[@id=\"js-pjax-container\"]/div/div[1]/div[2]/div/ul/div[1]/div[1]/h3/a/em")).getText();
+
+        String description = $(By.xpath("//*[@id=\"js-pjax-container\"]/div/div[1]/div[2]/div/ul/div[1]/div[1]/p")).getText();
+
+        String language1 = $(By.xpath("//*[@id=\"js-pjax-container\"]/div/div[1]/div[2]/div/ul/div[1]/div[2]")).getText();
+
+        String license1 = $(By.xpath("//*[@id=\"js-pjax-container\"]/div/div[1]/div[2]/div/ul/div[1]/div[1]/div/p[1]")).getText();
+
+        String stars1 = $(By.xpath("//*[@id=\"js-pjax-container\"]/div/div[1]/div[2]/div/ul/div[1]/div[3]/a")).getText();
+        if (namerepos.equals(Name.get(0))) {
+            System.out.println("Repository equals");
+        }
+        if (description.equals(Opys.get(0))) {
+            System.out.println("Description equals");
+        }
+        if (language1.equals(language.get(0))) {
+            System.out.println("Language equals");
+        }
+
+        if (license1.equalsIgnoreCase(license.get(0))) {
+            System.out.println("License equals");
+        }
+        if (Integer.parseInt(stars1) == (star.get(0))) {
+            System.out.println("Stars Equals");
+        }
+
+        JSONObject jsonResponse = new JSONObject(response.asString());
+        String capital = jsonResponse.getString("total_count");
+        Assert.assertEquals(capital, "527");
     }
 }
